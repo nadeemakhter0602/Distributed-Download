7481783bb161bd4a08575af535f17e1397519a05
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-06-07 21:47:09 +0530

    change requests session to set custom source address

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import netifaces
 from netifaces import AF_INET, ifaddresses
+import requests
 
 
 def get_ipaddrs():
@@ -13,3 +14,13 @@ def get_ipaddrs():
             ipaddr = addrs[AF_INET][0].get('addr')
             ipaddrs.append(ipaddr)
     return ipaddrs
+
+
+def custom_session(addr):
+    session = requests.Session()
+    for prefix in ('http://', 'https://'):
+        # modify pool manager of http adapter to use custom source address
+        session.get_adapter(prefix).init_poolmanager(connections=requests.adapters.DEFAULT_POOLSIZE,
+                                                     maxsize=requests.adapters.DEFAULT_POOLSIZE,
+                                                     source_address=(addr, 0))
+    return session
