01a542e8b6279cb3581bd0722b22bf8b4825feb0
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-06-12 20:53:42 +0530

    use https module, make all args required, set addresses

--- client.js
+++ client.js
@@ -1,18 +1,21 @@
 const util = require("node:util");
-const http = require("http");
+const https = require("https");
 const args = process.argv.slice(2);
 const options = {
     interface: {
         type: "string",
             short: "i",
+            required: true,
     },
     server: {
         type: "string",
         short: "s",
+        required: true,
     },
     url: {
         type: "string",
         short: "u",
+        required: true,
     },
 };
 let localAddress;
@@ -26,9 +29,24 @@ try {
         args,
         options,
     });
+    // check if all required arguments are present
+    for (let [name, {
+            required
+        }] of Object.entries(options)) {
+        if (required && !(name in values)) {
+            throw new Error(`--${name} must be provided`);
+        }
+    }
     localAddress = options.interface;
     serverAddress = options.server;
     downloadURL = options.url;
 } catch (error) {
     console.log(error.message);
 }
+let httpsOptions = {
+    localAddress: localAddress
+};
+const registerURL = serverAddress + '/register';
+const setFileInfoURL = serverAddress + '/setfileinfo';
+const getRangeURL = serverAddress + '/getrange';
+const mergeURL = serverAddress + '/merge';
