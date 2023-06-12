e2aa2ece9c22ac64c613676684f6956fef855025
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-06-12 21:54:23 +0530

    add both http and https modules, fix argument parsing, add register request

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const util = require("node:util");
+const http = require("http");
 const https = require("https");
 const args = process.argv.slice(2);
 const options = {
@@ -34,19 +35,38 @@ try {
             required
         }] of Object.entries(options)) {
         if (required && !(name in values)) {
-            throw new Error(`--${name} must be provided`);
+            console.log(`--${name} must be provided`);
+            process.exit();
         }
     }
-    localAddress = options.interface;
-    serverAddress = options.server;
-    downloadURL = options.url;
+    localAddress = values.interface;
+    serverAddress = values.server;
+    downloadURL = values.url;
 } catch (error) {
     console.log(error.message);
 }
-let httpsOptions = {
-    localAddress: localAddress
+let httpOptions = {
+    localAddress: localAddress,
 };
-const registerURL = serverAddress + '/register';
-const setFileInfoURL = serverAddress + '/setfileinfo';
-const getRangeURL = serverAddress + '/getrange';
-const mergeURL = serverAddress + '/merge';
+const registerURL = serverAddress + "/register";
+const setFileInfoURL = serverAddress + "/setfileinfo";
+const getRangeURL = serverAddress + "/getrange";
+const mergeURL = serverAddress + "/merge";
+const registerRequest = http.get(registerURL, (res) => {
+    const responseArray = [];
+    res.on("data", (chunk) => {
+        responseArray.push(chunk);
+    });
+    res.on("error", (err) => {
+        console.log(err);
+    });
+    res.on("end", () => {
+        const response = JSON.parse(responseArray.join(""));
+        if ("token" in response) {
+            const token = response["token"];
+            console.log(token);
+        } else {
+            console.log(response);
+        }
+    });
+});
